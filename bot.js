e3e4ac16cdd8b8373fc61156fe7981e3300ae919
Stecuuu <76130171+Stecuuu@users.noreply.github.com>
2020-12-25 01:03:55 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzgyMzgyOTkzOTU1NjE4ODY2.X8LY8Q.HL7vKfi93BaJ_AwrOQ4QkO_KQfI',
+        prefix: '^^',
+        activity: 'Widzowie sram do gara!!',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
